6014ccca5cc72991c8713e44d4de306635fdc485
Pedro Iaki <PedroAumsa@gmail.com>
2026-09-04 18:45:37 -0300

    masteries ingestion implemented

---
+++ league_pipeline/models/bronze/masteries.sql
@@ -0,0 +1,27 @@
+{{ config(
+    materialized='incremental',
+    file_format='delta',
+    incremental_strategy='merge',
+    unique_key=['puuid', 'state_hash'],
+    on_schema_change='append_new_columns'
+) }}
+
+with source_data as (
+    select
+        * except(_source_file, _ingested_at),
+        sha2(
+            concat_ws('|',
+                coalesce(region, ''), coalesce(championId, ''),
+                coalesce(patch, ''), coalesce(date, '')
+            ), 256
+        ) as state_hash,
+        _ingested_at
+    from {{ source('raw', 'masteries') }}
+)
+
+select *
+from source_data
+qualify row_number() over (
+    partition by puuid, state_hash
+    order by _ingested_at desc
+) = 1
